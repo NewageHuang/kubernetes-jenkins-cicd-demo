59d3278921fb26a3a0db0b49d0cc5f7e5494e71b
nathan-huang <nathan.huang@pwc.com>
2018-05-21 21:04:56 +0800

    change docker file

--- Dockerfiles/app/main.py
+++ Dockerfiles/app/main.py
@@ -1,14 +1,5 @@
 # coding: utf-8
 
-import numpy as np
-from nltk import word_tokenize
-from nltk.stem import WordNetLemmatizer
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-import itertools
-import pickle
-import scipy.sparse as sp
 from flask import Flask, jsonify, request, app
 
 # port number
@@ -17,11 +8,13 @@ web_port_number = 5002
 # start the application
 app = Flask(__name__)
 
+
 # default home page & API instructions
 @app.route('/')
 def instruction():
     instruction = "<br>This is kubernetes jenkins CI/CD demo.</br>"
     return instruction
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=web_port_number, debug=True)
